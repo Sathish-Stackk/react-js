28652cee4d9d0a5cdd18d78e008af886c7832cb4
Mungi Sathish <sathishmungi@gmail.com>
2026-09-01 21:58:21 +0530

    Create List_render.js

---
+++ List_render.js
@@ -0,0 +1,14 @@
+function FruitList() {
+  const fruits = ["Apple", "Banana", "Mango", "Grapes"];
+
+  return (
+    <div>
+      <h2>Fruit List</h2>
+      <ul>
+        {fruits.map((fruit, index) => (
+          <li key={index}>{fruit}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
